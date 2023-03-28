5a9b68a8d9a95a7b1f652ee807cb291f6a26f485
Shruti <shruti@simformsolutions.com>
2023-03-28 11:11:42 +0530

    First commit

---
+++ src/EmployeeE.java
@@ -0,0 +1,18 @@
+class EmployeeE {
+    //Private fields
+    private int emplID;
+    private  String name;
+    //Getter and setter methods
+    public void setEmplID(int emplID){
+        this.emplID = emplID;
+    }
+    public int getEmplId(){
+        return emplID;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+}
