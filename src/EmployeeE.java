ffde48ddeb9f12f37a633c0c199e2e1a04c7c776
Shruti <shruti@simformsolutions.com>
2023-04-03 13:50:57 +0530

    Update

--- src/EmployeeE.java
+++ src/EmployeeE.java
@@ -1,18 +1,22 @@
 class EmployeeE {
     //Private fields
     private int emplID;
-    private  String name;
+    private String name;
+
     //Getter and setter methods
-    public void setEmplID(int emplID){
+    public void setEmplID(int emplID) {
         this.emplID = emplID;
     }
-    public int getEmplId(){
+
+    public int getEmplId() {
         return emplID;
     }
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
 }
--- src/Encapsulation.java
+++ src/Encapsulation.java
@@ -1,16 +1,11 @@
-class Encapsulation{
+class Encapsulation {
     public static void main(String[] args) {
         //Class object creation
         EmployeeE emp = new EmployeeE();
         System.out.println("--- Employee Info ---");
         emp.setEmplID(1);
         emp.setName("Shruti");
-        System.out.print("ID : "+emp.getEmplId()+"  ");
-        System.out.println("Name : "+emp.getName());
-
-        emp.setEmplID(2);
-        emp.setName("Swati");
-        System.out.print("ID : "+emp.getEmplId()+"  ");
-        System.out.println("Name : "+emp.getName());
+        System.out.print("ID : " + emp.getEmplId() + "  ");
+        System.out.println("Name : " + emp.getName());
     }
 }
