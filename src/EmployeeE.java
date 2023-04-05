9bf20c2f37e40726b2c4d653f2918c3d17f378ac
Shruti <shruti@simformsolutions.com>
2023-04-05 12:36:58 +0530

    Removed Cooments

--- src/EmployeeE.java
+++ src/EmployeeE.java
@@ -1,9 +1,7 @@
 class EmployeeE {
-    //Private fields
     private int emplID;
     private String name;
 
-    //Getter and setter methods
     public void setEmplID(int emplID) {
         this.emplID = emplID;
     }
--- src/Encapsulation.java
+++ src/Encapsulation.java
@@ -1,6 +1,5 @@
 class Encapsulation {
     public static void main(String[] args) {
-        //Class object creation
         EmployeeE emp = new EmployeeE();
         System.out.println("--- Employee Info ---");
         emp.setEmplID(1);
